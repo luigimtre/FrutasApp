d08a7735a5e466aad0e632fa318287b9a1e1e940
luigimtre <luigimtre@alu.edu.gva.es>
2025-12-15 10:53:37 +0100

    Implementacion añadirFruta

--- src/FrutasApp.java
+++ src/FrutasApp.java
@@ -18,7 +18,7 @@ import java.util.Scanner;
                         verFrutas(frutas);
                         break;
                     case 2:
-                        System.out.println("Función aún no implementada.");
+                        añadirFruta(frutas,sc);
                         break;
                     case 3:
                         System.out.println("Función aún no implementada.");
@@ -74,7 +74,14 @@ import java.util.Scanner;
         /* ---------- Funciones pendientes ---------- */
 
         public static void añadirFruta(ArrayList<String> frutas, Scanner sc) {
-            // Pendiente
+            System.out.print("Nombre de la fruta a añadir: ");
+            String nombre = sc.nextLine();
+            if (frutas.contains(nombre)) {
+                System.out.println("Esa fruta ya existe.");
+            } else {
+                frutas.add(nombre);
+                System.out.println("Fruta añadida.");
+            }
         }
 
         public static void borrarFruta(ArrayList<String> frutas, Scanner sc) {
